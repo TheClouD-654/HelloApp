17b3488152cff568464f10c9614e357ec5ebcc02
TheClouD-654 <arunangshucloud@gmail.com>
2026-03-23 14:30:47 +0530

    Coded to display mmmmultiple naes

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,12 @@
 public class HelloApp {
-    void main(String[] args) {
-        System.out.println("Hello, World!");
+    StringBuilder namebuilder = new StringBuilder();
+    void main(String[] args, String name) {
+        for (int i=0; i<args.length; i++) {
+            namebuilder.append(args[i]);
+            if (i < args.length - 1) {
+                namebuilder.append(", ");
+            }
+            name = namebuilder.toString();
+        }
     }
 }
