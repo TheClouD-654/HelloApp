2d2601a348f7c1145815ae5381133ebc0dcd3b99
TheClouD-654 <arunangshucloud@gmail.com>
2026-03-25 23:02:54 +0530

    feature\UC6-substring-method

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 public class HelloApp {
-    public static void main(String[] args) {
+    private String name;
+    String result;
+
+    public void getnames(){
+        Scanner input= new Scanner (System.in);
+        System.out.println("Enter any many names you want: ");
+        name= input.nextLine();
+        String[] names=name.split(" ");
+        result = String.join(", ", names);
+        input.close();
+    }
+    public void display(){
         
-        StringBuilder nameBuilder = new StringBuilder();
-        for (String input:args) {
-            nameBuilder.append(input).append(", ");
-        }
-        String name;
-        if (nameBuilder.length()>0) {
-            System.out.println("Enter your name:");
-            S
-            name = nameBuilder.substring(0, nameBuilder.length() - 2);
-        }
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello "+result+"!");
+    }
+    public static void main(String[] args) {
+        HelloApp a=new HelloApp();
+        a.getnames();
+        a.display();
     }
 }
